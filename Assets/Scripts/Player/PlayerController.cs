d02929a8a46be65f3e0339794add0933d35b7295
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-19 20:43:16 -0300

    build: v0.0.1-dev

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float velocity;
+    private float speed;
+    
     public float mass;
+    
     public float turnSpeed;
     public TMP_Text speedmeter;
 
     public float horizontalInput;
     public float verticalInput;
+
+    public Rigidbody car;
+
+    public bool showSpeed;
 
     void Start()
     {
@@ -21,12 +29,31 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+
+        speed = car.velocity.x * 3.6f;
+
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
+        if (verticalInput != 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        }
 
         transform.Translate(Vector3.forward * Time.deltaTime * velocity * verticalInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
-        speedmeter.text = "Km/H " + velocity.ToString();
+
+
+        if(Input.GetKeyDown(KeyCode.F9))
+        {
+            showSpeed = !showSpeed;
+        }
+
+        if(showSpeed)
+        {
+            speedmeter.text = "Km/H " + speed.ToString();
+        }else
+        {
+            speedmeter.text = "";
+        }
     }
 }
