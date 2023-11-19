1a27f15a5f8097ba5ad3e3978772ea837fff8e76
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-19 19:15:38 -0300

    feat: toogle fps display on production mode

--- Assets/Scripts/UI/FPSDisplay.cs
+++ Assets/Scripts/UI/FPSDisplay.cs
@@ -8,14 +8,25 @@ public class FPSDisplay : MonoBehaviour
     public TMP_Text fpsText;
     public float deltaTime;
     public bool showFPS = false;
+ 
 
     void Update()
     {
+       if(Input.GetKeyDown(KeyCode.F9))
+        {
+            showFPS = !showFPS;
+        }
+
         if (showFPS)
         {
             deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
             float fps = 1.0f / deltaTime;
             fpsText.text = "FPS: " + Mathf.Ceil(fps).ToString();
+        } else
+        {
+            fpsText.text = "";
         }
+
+        
     }
 }
