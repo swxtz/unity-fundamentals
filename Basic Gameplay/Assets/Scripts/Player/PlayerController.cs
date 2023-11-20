1a1a65a129c0b62a7442c78f2d06a60b03359a21
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-20 15:58:53 -0300

    feat: create simple thirt person controller

--- Basic Gameplay/Assets/Scripts/Player/PlayerController.cs
+++ Basic Gameplay/Assets/Scripts/Player/PlayerController.cs
@@ -4,49 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Transform orietation;
 
     private float horizontalInput;
     private float verticalInput;
 
-    private float mouseY;
-    private float mouseX;
+    private float speed = 10.0f;
 
-    private float xRotation;
-    private float yRotation;
-
-    public float speed = 10.0f;
-    public float sensi = 10.0f;
-
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
+    private int xRange = 24;
 
 
     void Update()
     {
+
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-
-        mouseY = Input.GetAxisRaw("Mouse Y");
-        mouseX = Input.GetAxisRaw("Mouse X");
 
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
-        yRotation += mouseX;
+    }
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    void LateUpdate()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
 
-        //transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orietation.rotation = Quaternion.Euler(0, yRotation, 0);
-
-
-
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
