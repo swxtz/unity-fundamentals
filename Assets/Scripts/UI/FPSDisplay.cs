e56ef68d9e768a081a10e8f04acdc85154c0f2f9
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-19 21:40:17 -0300

    feat: fps display

---
+++ Assets/Scripts/UI/FPSDisplay.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public TMP_Text fpsText;
+    public float deltaTime;
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = "FPS: " + Mathf.Ceil(fps).ToString();
+    }
+}
