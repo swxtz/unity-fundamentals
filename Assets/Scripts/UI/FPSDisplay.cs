725eaab4c5c1e1520d58dabaa172a55ca6427395
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-19 21:42:25 -0300

    feat: fps display when debug mode

--- Assets/Scripts/UI/FPSDisplay.cs
+++ Assets/Scripts/UI/FPSDisplay.cs
@@ -7,11 +7,15 @@ public class FPSDisplay : MonoBehaviour
 {
     public TMP_Text fpsText;
     public float deltaTime;
+    public bool showFPS = false;
 
     void Update()
     {
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-        float fps = 1.0f / deltaTime;
-        fpsText.text = "FPS: " + Mathf.Ceil(fps).ToString();
+        if (showFPS)
+        {
+            deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+            float fps = 1.0f / deltaTime;
+            fpsText.text = "FPS: " + Mathf.Ceil(fps).ToString();
+        }
     }
 }
