03eb08fb844115508af42d5b8b34de40fbe60fe2
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-25 22:15:47 -0300

    feat: create spanw manager

---
+++ Basic Gameplay/Assets/Scripts/SpawnManeger/SpawnManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalsPrefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}
