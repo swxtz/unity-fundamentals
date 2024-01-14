10b5c7fa50206d4485f4a0799b035b8610178734
Gustavo Mendonça <alf4r6@gmail.com>
2024-01-14 13:38:10 -0300

    feat: add auto spawner in spawn maganer

--- Basic Gameplay/Assets/Scripts/SpawnManeger/SpawnManager.cs
+++ Basic Gameplay/Assets/Scripts/SpawnManeger/SpawnManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
@@ -11,14 +12,18 @@ public class SpawnManager : MonoBehaviour
     private float xRange = 20;
     private float zPos = 20;
 
+    private float startInterval = 2;
+    private float spawnInverval = 1.5f;
 
-    void Update()
+    void Start()
     {
-       if(Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalsPrefab.Length);
-            Vector3 randPos = new Vector3(Random.Range(-xRange, xRange), 0, zPos);
-            Instantiate(animalsPrefab[animalIndex], randPos, animalsPrefab[animalIndex].transform.rotation);
-        } 
+        InvokeRepeating("SpawnRandomAnimal", startInterval, spawnInverval);    
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalsPrefab.Length);
+        Vector3 randPos = new Vector3(Random.Range(-xRange, xRange), 0, zPos);
+        Instantiate(animalsPrefab[animalIndex], randPos, animalsPrefab[animalIndex].transform.rotation);
     }
 }
