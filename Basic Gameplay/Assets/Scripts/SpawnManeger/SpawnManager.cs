b55d7d229a2ee932807c0d1cc352c35c064faf45
Gustavo Mendonça <alf4r6@gmail.com>
2024-01-14 13:18:03 -0300

    feat: create spawn manager

--- Basic Gameplay/Assets/Scripts/SpawnManeger/SpawnManager.cs
+++ Basic Gameplay/Assets/Scripts/SpawnManeger/SpawnManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalsPrefab;
 
+    // random position values
+    private float xRange = 20;
+    private float zPos = 20;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+       if(Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalsPrefab.Length);
+            Vector3 randPos = new Vector3(Random.Range(-xRange, xRange), 0, zPos);
+            Instantiate(animalsPrefab[animalIndex], randPos, animalsPrefab[animalIndex].transform.rotation);
+        } 
     }
 }
